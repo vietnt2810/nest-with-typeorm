ebec6cd0280eefa4e6a3477ae7d6003d9475f341
viet.nguyen3 <viet.nguyen3@monstar-lab.com>
2023-09-29 17:37:29 +0700

    Add users method

--- src/entities/users/users.controller.ts
+++ src/entities/users/users.controller.ts
@@ -1,16 +1,35 @@
 import { CreateUserDto } from './dtos/createUser.dto';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/updateUser.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
-
   @Get()
-  //   getUsers() {}
-  @Post()
+  getUsers() {
+    return this.usersService.findUsers();
+  }
+
+  @Post('create')
   createUser(@Body() createUserDto: CreateUserDto) {
     this.usersService.createUser(createUserDto);
-    return 'yo';
+    return 'Created new user successfully';
+  }
+
+  @Patch(':id')
+  updateUserById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    this.usersService.updateUser(id, updateUserDto);
   }
 }
--- src/entities/users/users.service.ts
+++ src/entities/users/users.service.ts
@@ -2,17 +2,26 @@ import { Users } from './users.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateUserParams } from './types/users.type';
+import { CreateUserParams, UpdateUserParams } from './types/users.type';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users) private userRepository: Repository<Users>,
   ) {}
+
+  findUsers() {
+    return this.userRepository.find();
+  }
+
   createUser(createUserParams: CreateUserParams) {
     const newUser = this.userRepository.create({
       ...createUserParams,
     });
     return this.userRepository.save(newUser);
+  }
+
+  updateUser(id: number, updateUserParams: UpdateUserParams) {
+    return this.userRepository.update({ id }, { ...updateUserParams });
   }
 }
